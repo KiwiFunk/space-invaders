2f2156f988dbced49e8d09ee4cf5d4b2bc5e9df8
Alex <alex_p97@live.com>
2025-02-03 11:48:24 +0000

    Started on game loop, added listeners for player input

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,20 +3,47 @@ import Player from './player.js';
 import Invader from './invaders.js';
 
 //Global Variables
+let player;
+
+//Game State bools
+let gameStarted = false;
+let gameEnded = false;
+
 
 //Game Loop
-
 document.addEventListener('DOMContentLoaded', function() {
 
+    //Init game loop on player input
+    document.addEventListener('keydown', function(e) {
+        //Get playable area
+        const gameArea = document.getElementById('gameArea');
 
+        if(!gameStarted) {
+            //Start moving invaders
+            gameStarted = true;
+        }
 
+        //Player Inputs
+        if (e.key === 'ArrowLeft' || e.key === 'a') {
+            if (player.xpos > 0) {  // Ensure player doesn't move out of bounds
+                player.move(-10, 0);  // Move left
+            }
+        }
 
+        if (e.key === 'ArrowRight' || e.key === 'd') {
+            if (player.xpos < gameArea.clientWidth - player.element.offsetWidth) {  // Ensure player stays within bounds
+                player.move(10, 0);  // Move right
+            }
+        }
 
+        if (e.key === ' ') {
+            //Fire player bullet
+        }
 
-
-
+    });
 
 });
+
 
 //Game Functions
 
